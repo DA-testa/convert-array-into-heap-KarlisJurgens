4b9b02ac2f191b12b3075921bb333bab7882c3f3
Kārlis Jurgens <124631657+KarlisJurgens@users.noreply.github.com>
2023-03-16 16:48:00 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,44 @@
-# python3
-
-
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
+    sorted = False
+    i=1
+    j=2
+    n=len(data)
+    while sorted == False :
+        while j<=i*2+1:
+            if data[i-1]<data[j-1]:
+                if j<n:
+                    j+=1
+                else:
+                    sorted= True
+                    break
+                
+            elif data[i-1]>data[j-1]:
+                k=data[i-1]
+                data[i-1]=data[j-1]
+                data[j-1]=k
+                swaps.append((i,j))
+                i=1
+                j=2
+        if j==n:
+            sorted= True
+            break
+        i=i+1
+        j=i*2
 
     return swaps
 
-
-def main():
-    
+def main():  
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
+    
     # input from keyboard
-    n = int(input())
+    n = int(input()) 
+    assert 1<=n<=100000,"n out of bounds"
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
